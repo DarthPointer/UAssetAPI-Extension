33746a939e0eb988b2150ff1d56b7e309258030d
DarthPointer <al.kiriackin@yandex.ru>
2022-02-26 19:58:53 +0300

    First Push of Code
    ## Added:
    - `UAssetAPI.Extension` namespace. Everything is defined there (at least yet).
    - Public static class `UAssetExtension` with extenstion methods for UAAPI's `UAsset` class.
    - `NameReference.cs`. `INameReference` to wrap everything that can be used to refer a Name Map entry.
    - `ObjectReference.cs`. Interfaces and their implementations there to refer Imports/Exports.
    - `NameData.cs`. `NameData` as a wrapper to refer UAAPI's `FName`. Can also refer Imports/Exports via their object names.
    - `PreloadDependency.cs`. Contains enumeration for types of preload dependencies to refer when accessing that list.

---
+++ Source/UAssetAPI-Extenstion/UAssetAPI-Extension/NameData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace UAssetAPI.Extension
+{
+    public class NameData : FName, IObjectReference, IImportReference, IExportReference
+    {
+        public NameData(INameReference name, UAsset asset, int number = 0) : base(name.GetFString(asset), number) { }
+
+        /// <summary>
+        /// Generates an FName that exists in the <paramref name="asset"/> and this <see cref="NameData"/> refers.
+        /// Throws otherwise.
+        /// </summary>
+        /// <param name="asset">Context asset that relevant entries are stored in.</param>
+        /// <returns></returns>
+        public FName AsAssetFName(UAsset asset)
+        {
+            return this;
+        }
+
+        FPackageIndex IObjectReference.GetObjectIndex(UAsset asset)
+        {
+            int importIndex = asset.SearchForImport(this);
+            if (importIndex < 0) return new FPackageIndex(importIndex);
+
+            int exportIndex = asset.SearchForExport(this);
+            if (exportIndex > 0) return new FPackageIndex(exportIndex);
+
+            throw new InvalidOperationException($"Name Data refers an object called `{this}` which is not present in the asset");
+        }
+
+        Import IImportReference.GetImport(UAsset asset)
+        {
+            return asset.GetImport(this);
+        }
+
+        Export IExportReference.GetExport(UAsset asset)
+        {
+            return asset.GetExport(this);
+        }
+    }
+}
